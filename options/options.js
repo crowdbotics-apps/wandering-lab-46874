6cb5dd3391b2ea1e7a325de432a47f16f32dee8e
Crowdbotics <team@crowdbotics.com>
2024-03-08 11:38:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-lab-46874",
+  url: "https://wandering-lab-46874.botics.co",
+  api: "https://wandering-lab-46874.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
